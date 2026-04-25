5f0fe6c9c7a699c3670b548dcdeae782028d2785
MarinaGolombieski <marina.golombieski.dev2025@gmail.com>
2026-04-25 14:46:17 -0300

    Update ClienteRequestDTO.java

--- prjCafeDev/src/main/java/com/marina/br/dtos/ClienteRequestDTO.java
+++ prjCafeDev/src/main/java/com/marina/br/dtos/ClienteRequestDTO.java
@@ -4,11 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public class ClienteRequestDTO {
-	@NotBlank
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
 	
-	@Email
-    @NotBlank
+	@Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
 	private String email;
 	
 	
